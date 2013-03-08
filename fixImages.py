0094f17a807c21fa893f57ec688d02173667facb
Mario Balibrera <mario@gameclosure.com>
2013-03-08 13:57:11 -0800

    batch edit images with black for transparency: new script; modified images.

---
+++ fixImages.py
@@ -0,0 +1,12 @@
+import os
+from subprocess import call
+
+def process(imgs, dirname, fnames):
+	print "replacing black backgrounds with transparency"
+	for img in imgs:
+		hits = [os.path.join(dirname, fname) for fname in fnames if fname.startswith(img)]
+		for fpath in hits:
+			print ' - converting', fpath
+			call('convert -transparent black -fuzz 10%% %s %s'%(fpath, fpath), shell=True)
+
+os.path.walk('resources/images', process, ['explosion1', 'explosion3', 'playerthrust'])
