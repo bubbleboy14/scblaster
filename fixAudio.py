9ea26f1f1522b9b485db3754419d219a5c5e4b69
Mario Balibrera <mario@gameclosure.com>
2013-03-08 17:09:35 -0800

    audio fixing: retain m4a's for browser mode

--- fixAudio.py
+++ fixAudio.py
@@ -1,10 +1,7 @@
 from subprocess import call
 
-print 'removing redundant m4a files'
-call('rm resources/media/*m4a', shell=True)
-
 print 'renaming necessary ogg files to mp3 and sanitizing'
 call('rename -s ogg mp3 resources/media/*ogg -z', shell=True)
 
 print 'replacing dashes with underscores'
-call('rename -s - _ resources/media/*', shell=True)
+call('rename -s - _ resources/media/*mp3', shell=True)
