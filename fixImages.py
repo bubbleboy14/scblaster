646e1fcb1f05613401edc7205f17d7737408de06
Mario Balibrera <mario@gameclosure.com>
2013-03-08 18:38:18 -0800

    force png32

--- fixImages.py
+++ fixImages.py
@@ -7,7 +7,7 @@ def process(imgs, dirname, fnames):
 		hits = [os.path.join(dirname, fname) for fname in fnames if fname.startswith(img)]
 		for fpath in hits:
 			print ' - converting', fpath
-			call('convert -transparent black -fuzz 10%% %s %s'%(fpath, fpath), shell=True)
+			call('convert -transparent black -fuzz 10%% %s png32:%s'%(fpath, fpath), shell=True)
 
 def mono(nothing, dirname, fnames):
 	for fname in [f for f in fnames if f.endswith('png')]:
