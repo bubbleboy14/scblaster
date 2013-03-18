c22df033dd4b493f15332384088eea06d35bec25
Mario Balibrera <mario@gameclosure.com>
2013-03-18 15:50:02 -0700

    renamed variables in fixImages for clarity

--- fixImages.py
+++ fixImages.py
@@ -1,10 +1,10 @@
 import os
 from subprocess import call
 
-def process(imgs, dirname, fnames):
+def process(targets, dirname, fnames):
 	print "replacing black backgrounds with transparency"
-	for img in imgs:
-		hits = [os.path.join(dirname, fname) for fname in fnames if fname.startswith(img)]
+	for target in targets:
+		hits = [os.path.join(dirname, fname) for fname in fnames if fname.startswith(target)]
 		for fpath in hits:
 			print ' - converting', fpath
 			call('convert -transparent black -fuzz 10%% %s png32:%s'%(fpath, fpath), shell=True)
