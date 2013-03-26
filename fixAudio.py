6e1273234d9d2a431b6cbbf3233c96707c35e048
Mario Balibrera <mario@gameclosure.com>
2013-03-25 19:33:15 -0700

    fixAudio: don't change file extensions

--- fixAudio.py
+++ fixAudio.py
@@ -1,7 +1,4 @@
 from subprocess import call
 
-print 'renaming ogg files to mp3 and sanitizing'
-call('rename -s ogg mp3 resources/media/*ogg -z', shell=True)
-
-print 'replacing dashes with underscores'
-call('rename -s - _ resources/media/*mp3', shell=True)
+print 'replacing dashes with underscores and sanitizing'
+call('rename -s - _ resources/media/*ogg -z', shell=True)
